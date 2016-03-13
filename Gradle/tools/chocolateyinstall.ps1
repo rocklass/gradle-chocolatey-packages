27bcff1e4766d12cb917e12ed4d3e4b15b973e75
Daniel Schröder <mr.danielfsch@gmail.com>
2016-03-13 00:54:49 -0300

    Add chocolateyinstall.ps1 for gradle package

---
+++ Gradle/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$binRoot = Get-BinRoot
+
+$name = 'Gradle'
+$version = '2.11'
+
+$gradle_home = Join-Path $binRoot $name
+$gradle_bin = Join-Path $gradle_home 'bin'
+
+$url = https://services.gradle.org/distributions/gradle-$version-all.zip
+
+Install-ChocolateyZipPackage $name $url $binRoot
+
+Install-ChocolateyPath $gradle_bin 'User'
