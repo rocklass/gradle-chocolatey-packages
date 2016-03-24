fd08b7b458874b5c987df5c96cb037e41aeaab60
Daniel Schröder <mr.danielfsch@gmail.com>
2016-03-24 20:16:31 -0300

    Fix bugs in nuspec file and install script. Add basic uninstall script

--- Gradle/tools/chocolateyinstall.ps1
+++ Gradle/tools/chocolateyinstall.ps1
@@ -1,13 +1,18 @@
 ﻿$binRoot = Get-BinRoot
 
 $name = 'Gradle'
-$version = '2.11'
+$version = $env:chocolateyPackageVersion
+
+$extract_path = Join-Path $binRoot "$($name)-$version"
 
 $gradle_home = Join-Path $binRoot $name
 $gradle_bin = Join-Path $gradle_home 'bin'
 
-$url = https://services.gradle.org/distributions/gradle-$version-all.zip
+$url = "https://services.gradle.org/distributions/gradle-$version-bin.zip"
 
 Install-ChocolateyZipPackage $name $url $binRoot
 
-Install-ChocolateyPath $gradle_bin 'User'
+Rename-Item $extract_path $name
+
+Install-ChocolateyEnvironmentVariable "GRADLE_HOME" $gradle_home 'Machine'
+Install-ChocolateyPath $gradle_bin 'Machine'
