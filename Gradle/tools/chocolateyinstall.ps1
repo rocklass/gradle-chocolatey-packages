c620d94f5403be1ca2b69f3b23f5265146c6d641
Daniel Schröder <mr.danielfsch@gmail.com>
2017-03-17 04:16:46 -0300

    Upgrade gradle package to version 3.4.1

--- Gradle/tools/chocolateyinstall.ps1
+++ Gradle/tools/chocolateyinstall.ps1
@@ -1,15 +1,6 @@
-﻿
-[string]$chocoVersion = $(choco --version)
-[string]$majorRelease = $chocoVersion.Split('.')[1]
-[string]$minorRelease = $chocoVersion.Split('.')[2]
-if (-not $majorRelease.Equals("9") -or -not $minorRelease.Equals("9"))
-{
-    write-host "WARNING: This package has only been tested with Chocolatey 0.9.9" -f "Yellow"
-}
-
-$packageName = 'gradle'
+﻿$packageName = 'gradle'
 $version = $env:chocolateyPackageVersion
-$checksum = 'C7DE3442432253525902F7E8D7EAC8B5FD6CE1623F96D76916AF6D0E383010FC'
+$checksum = 'DB1DB193D479CC1202BE843F17E4526660CFB0B21B57D62F3A87F88C878AF9B2'
 $url = "https://services.gradle.org/distributions/gradle-$version-bin.zip"
 $installDir = Split-Path -parent $MyInvocation.MyCommand.Definition
 
