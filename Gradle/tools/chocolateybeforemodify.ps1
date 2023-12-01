54dfa4df6018d6e44198fa6bf3c3447acc565120
Vincent Biret <vincentbiret@hotmail.com>
2023-12-01 08:31:33 -0500

    - upgrades to gradle 8.5

--- Gradle/tools/chocolateybeforemodify.ps1
+++ Gradle/tools/chocolateybeforemodify.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName    = $env:chocolateyPackageName
-$packageVersion = "8.4"
+$packageVersion = "8.5"
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation   = Join-Path $toolsDir "$packageName-$packageVersion"
 
